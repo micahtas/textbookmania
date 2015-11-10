63e4dffba45f77453c89a84a720316f710d5ff72
Micah <micahtas@hawaii.edu>
2015-11-10 09:47:20 -1000

    practice WoD

---
+++ app/lib/router/Router.js
@@ -0,0 +1,44 @@
+/**
+ * Configure Iron Router.
+ * See: http://iron-meteor.github.io/iron-router/
+ */
+
+
+Router.configure({
+  layoutTemplate: 'Layout',
+  waitOn: function() { return Meteor.subscribe("Stuff"); },
+  loadingTemplate: 'Loading'
+});
+
+Router.route('/', {
+  name: 'Home'
+});
+
+Router.route('/students', {
+  name: 'Students'
+});
+
+Router.route('/textbooks', {
+  name: 'Textbooks'
+});
+
+Router.route('/buyorders', {
+  name: 'BuyOrders'
+});
+
+Router.route('/sellorders', {
+    name: 'SellOrders'
+});
+
+Router.route('/matches', {
+    name: 'Matches'
+});
+
+Router.route('/help', {
+    name: 'Help'
+});
+
+Router.route('/stuff/:_id', {
+  name: 'EditStuff',
+  data: function() { return Stuff.findOne(this.params._id); }
+});
